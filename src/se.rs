379cc56a5eccbd1a575b0995388ab6c689442116
mimi <dev@zheteng.cloud>
2021-08-07 05:45:29 +0800

    Correct xmlns and version

--- src/se.rs
+++ src/se.rs
@@ -8,27 +8,40 @@ trait Serializer {
     fn to_xml(&self, resp: &Response) -> Result<String, Box<dyn Error>>;
 }
 
-pub struct Itune {
-    version: String,
-    xmlns: String,
+pub struct Itune<'a> {
+    version: &'a str,
+    version_val: &'a str,
+    xmlns: &'a str,
+    xmlns_val: &'a str,
 }
 
 static RSS: &str = "rss";
 static CHANNEL: &str = "channel";
 static TITLE: &str = "title";
+static DTD1: &str = "http://www.itunes.com/dtds/podcast-1.0.dtd";
 
-impl Serializer for Itune {
-    fn to_xml(&self, resp: &Response) -> Result<String, Box<dyn Error>> {
+impl<'a> Serializer for Itune<'a> {
+    fn to_xml(&self, _resp: &Response) -> Result<String, Box<dyn Error>> {
         //ascii space 32
         let mut writer = Writer::new_with_indent(Cursor::new(Vec::new()), 32u8, 2);
         writer.write_event(Event::Decl(BytesDecl::new(b"1.0", Some(b"UTF-8"), None)))?;
-        writer.write_event(Event::Start(BytesStart::owned(RSS.as_bytes(), RSS.len())))?;
+        //rss tag
+        let mut rss_tag = BytesStart::owned(RSS.as_bytes(), RSS.len());
+        rss_tag.push_attribute((self.xmlns, self.xmlns_val));
+        rss_tag.push_attribute((self.version, self.version_val));
+        writer.write_event(Event::Start(rss_tag))?;
 
         //channel
-        writer.write_event(Event::Start(BytesStart::owned(CHANNEL.as_bytes(), CHANNEL.len())))?;
+        writer.write_event(Event::Start(BytesStart::owned(
+            CHANNEL.as_bytes(),
+            CHANNEL.len(),
+        )))?;
 
         //title
-        writer.write_event(Event::Start(BytesStart::owned(TITLE.as_bytes(),TITLE.len())))?;
+        writer.write_event(Event::Start(BytesStart::owned(
+            TITLE.as_bytes(),
+            TITLE.len(),
+        )))?;
 
         writer.write_event(Event::End(BytesEnd::borrowed(TITLE.as_bytes())))?;
         writer.write_event(Event::End(BytesEnd::borrowed(CHANNEL.as_bytes())))?;
@@ -51,8 +64,10 @@ mod tests {
     #[test]
     fn se_xml() -> Result<(), Box<dyn Error>> {
         let itune = Itune {
-            xmlns: "dtd1".to_string(),
-            version: "2.0".to_string(),
+            xmlns: "xmlns:itunes",
+            xmlns_val: DTD1,
+            version: "version",
+            version_val: "2.0",
         };
         let json: String = fs::read_to_string("api_response.json")?;
         let response: Response = serde_json::from_str(&json)?;
