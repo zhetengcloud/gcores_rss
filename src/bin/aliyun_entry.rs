3490e01e8f9e60beb28e5534ffa99803d4611aed
mimi <allthatjazz@outlook.com>
2021-08-23 13:45:01 +0800

    Add access id secret fileds to OssParam

--- src/bin/aliyun_entry.rs
+++ src/bin/aliyun_entry.rs
@@ -21,13 +21,21 @@ async fn main() {
         .untuple_one()
         .and_then(|id, secret, req: req::Request| async move {
             let req::Request {
-                oss_param: _,
+                mut oss_param,
                 param,
                 channel,
             } = req;
             let xml: String = gcores_rss::get(param, channel).await.unwrap();
 
-            Ok::<String, warp::reject::Rejection>(format!("{},{}\ntitle:{}", id, secret, xml))
+            oss_param.access_id = Some(id);
+            oss_param.access_secret = Some(secret);
+
+            Ok::<(req::OssParam, String), warp::reject::Rejection>((oss_param, xml))
+        })
+        .untuple_one()
+        .and_then(|param, xml| async move {
+            req::save_to_oss(param, xml).await;
+            Ok::<String, warp::reject::Rejection>("saved to oss".to_string())
         });
 
     warp::serve(route).run(([0, 0, 0, 0], 9000)).await;
@@ -38,20 +46,28 @@ mod req {
     use gcores_rss::{Channel, Param};
     use serde::Deserialize;
 
+    pub async fn save_to_oss(param: OssParam, xml: String) {
+        unimplemented!()
+    }
+
     #[derive(Deserialize)]
     pub struct Request {
         #[serde(rename = "storage_param")]
-        pub oss_param: S3Param,
+        pub oss_param: OssParam,
         pub channel: Channel,
         pub param: Param,
     }
 
-    #[derive(Deserialize)]
-    pub struct S3Param {
+    #[derive(Deserialize, Debug)]
+    pub struct OssParam {
         pub service: String,
         pub bucket: String,
         pub key: String,
         pub acl: Option<String>,
         pub content_type: Option<String>,
+        #[serde(skip)]
+        pub access_id: Option<String>,
+        #[serde(skip)]
+        pub access_secret: Option<String>,
     }
 } /* req */
